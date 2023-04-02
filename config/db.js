0b19855a8d64e58e6eff7719392581c629a4fd18
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2023-04-02 23:45:35 +0530

    Mongo url fixed

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 // -------------- (MongoDb Connection) -------------
 const connect = () => {
-  return mongoose.connect(process.env.MONGO_URL)
+  mongoose.set("strictQuery", false);
+  return mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 module.exports = connect;
